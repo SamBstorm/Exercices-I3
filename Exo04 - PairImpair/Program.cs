7370f4be007fd2ab414bae602192b0ce53cb81ed
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-22 16:48:04 +0200

    Add Correction Exo04

---
+++ Exo04 - PairImpair/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exo04___PairImpair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez indiquer un nombre entier : ");
+
+            if (!int.TryParse(Console.ReadLine(), out int nb))
+            {
+                Console.WriteLine("La valeur n'est pas un nombre entier...");
+            }
+            else {
+                if ((nb / 2) * 2 == nb)
+                {
+                    Console.WriteLine($"Le nombre {nb} est pair.");
+                }
+                else
+                {
+                    Console.WriteLine($"Le nombre {nb} est impair.");
+                }
+            }
+        }
+    }
+}
