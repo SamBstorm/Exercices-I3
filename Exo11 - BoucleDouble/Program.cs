5e47e3cec6ac5af48cb2796d5b5cb4cc394fad1f
Legrain Samuel <samuel.legrain@bstorm.be>
2022-05-06 15:29:02 +0200

    Add Boucle : double

---
+++ Exo11 - BoucleDouble/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exo11___BoucleDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (decimal i = 0; i <= 20; i+=0.1M)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
