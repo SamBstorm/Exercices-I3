189e939c05e687d26b6efd97c8cbd7c29e7e6137
Legrain Samuel <samuel.legrain@bstorm.be>
2022-05-06 14:06:33 +0200

    Add Exo-Factoriel

---
+++ Exo08 - Factoriel/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exo08___Factoriel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint input;
+            do Console.WriteLine("Veuillez introduire un nombre :");
+            while (!uint.TryParse(Console.ReadLine(), out input));
+            //int factorial = 1;
+            //for (int i = 2; i <= input; i ++)
+            //{
+            //    factorial *= i;
+            //}
+            //Console.WriteLine($"La factoriel de {input} est {factorial}");
+            ulong factorial = 1;
+            for (uint i = input; i > 1; i--)
+            {
+                checked
+                {
+                    factorial *= i;
+                }
+            }
+            Console.WriteLine($"La factoriel de {input} est {factorial}");
+        }
+    }
+}
