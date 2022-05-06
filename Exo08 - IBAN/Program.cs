3956b93439255b2fb14ca4d530bafd2dfb4641e3
Legrain Samuel <samuel.legrain@bstorm.be>
2022-05-06 09:50:38 +0200

    Add Correction : IBAN

---
+++ Exo08 - IBAN/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exo08___IBAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez introduire votre numéro BBAN (12 chiffres) : ");
+            string bban = Console.ReadLine();
+            Console.Clear();
+            if (bban.Length == 12)
+            {
+                if (ulong.TryParse(bban, out _))
+                {
+                    string last_two = bban.Substring(10, 2);
+                    string code_str = last_two + last_two + 111400;
+                    ulong code = ulong.Parse(code_str);
+                    ushort first_two = (ushort)(98 - (code % 97));
+                    if (first_two < 10) Console.WriteLine($"BE0{first_two + bban}");
+                    else Console.WriteLine($"BE{first_two + bban}");
+                }
+                else {
+                    Console.WriteLine("Ceci n'est pas un BBAN valide : Paysan!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Shaila a dit que tu devais apprendre à lire!");
+            }
+        }
+    }
+}
