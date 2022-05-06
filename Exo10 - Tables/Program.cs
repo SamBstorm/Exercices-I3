7e92b95940030a50202aef14170dbbebda4fc348
Legrain Samuel <samuel.legrain@bstorm.be>
2022-05-06 14:50:21 +0200

    Add correction Table

---
+++ Exo10 - Tables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exo10___Tables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int table = 1; table <= 5; table++)
+            {
+                for (int multiplicateur = 1; multiplicateur <= 20; multiplicateur++)
+                {
+                    Console.Write($"{table} X {multiplicateur} = {table * multiplicateur} ; ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
