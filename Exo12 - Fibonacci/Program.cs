b5bfcaf40060e17a0852ede16156d21fba3b352d
Legrain Samuel <samuel.legrain@bstorm.be>
2022-05-10 09:39:34 +0200

    Add corrections : Fibonnaci et Premiers

---
+++ Exo12 - Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exo12___Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nb1 = 0, nb2 = 1, nb3;
+            Console.WriteLine($"F0 : {nb1}");
+            Console.WriteLine($"F1 : {nb2}");
+            for (int i = 2; i <= 25; i++)
+            {
+                nb3 = nb1 + nb2;
+                Console.WriteLine($"F{i} : {nb3}");
+                nb1 = nb2;
+                nb2 = nb3;
+            }
+        }
+    }
+}
---
+++ Exo13 - NombresPremiers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exo13___NombresPremiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint number;
+            do {
+                Console.WriteLine("Combien de nombres premiers voulez-vous?");   
+            }   while (!uint.TryParse(Console.ReadLine(), out  number));
+            for (int count = 0, nbTest = 2; count < number; nbTest++)
+            {
+                int div_count = 0; 
+                for (int diviseur = 2; diviseur < nbTest; diviseur++) {
+                    if (nbTest % diviseur == 0) div_count++;
+                }
+                if (div_count == 0) {
+                    count++;
+                    Console.WriteLine(nbTest);
+                }
+            }
+        }
+    }
+}
