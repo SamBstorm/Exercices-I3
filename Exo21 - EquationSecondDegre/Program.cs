760afc9ada64252d213d88cb102b893b17cfe170
Legrain Samuel <samuel.legrain@bstorm.be>
2022-05-24 09:47:59 +0200

    Add correction Exo21 - Equation Second degré

---
+++ Exo21 - EquationSecondDegre/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exo21___EquationSecondDegre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation eq;
+            eq.A = 1;
+            eq.B = 1;
+            eq.C = 1;
+            if(eq.Resoudre(out double? reponse1, out double? reponse2))
+            {
+                if(reponse1 == reponse2)  Console.WriteLine($"{eq.A} X² + {eq.B} X + {eq.C} , X peut être {reponse1}.");
+                else Console.WriteLine($"{eq.A} X² + {eq.B} X + {eq.C} , X peut être {reponse1} ou {reponse2}.");
+            }
+            else
+            {
+                Console.WriteLine($"{eq.A} X² + {eq.B} X + {eq.C} , n'est pas résolvable...");
+            }
+        }
+    }
+}
---
+++ Exo21 - EquationSecondDegre/Structs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo21___EquationSecondDegre
+{
+    public struct Equation
+    {
+        #region Champs/Variables membres
+
+        public double A;
+        public double B;
+        public double C;
+
+        #endregion
+        #region Méthodes
+        public bool Resoudre(out double? X1, out double? X2)
+        {
+            double delta = Math.Pow(B,2) - (4 * A * C);
+            if(delta < 0)
+            {
+                X1 = X2 = null;
+                return false;
+            }
+            X1 = (-B - Math.Sqrt(delta)) / (2 * A);
+            X2 = (-B + Math.Sqrt(delta)) / (2 * A);            
+            return true;
+        }
+        #endregion
+    }
+}
