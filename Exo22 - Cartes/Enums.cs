24090c1e198ae6f8af2661bf706a00b36fec895c
Legrain Samuel <samuel.legrain@bstorm.be>
2022-05-24 16:10:37 +0200

    Add Correction Exo 22 Cartes

---
+++ Exo22 - Cartes/Enums.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo22___Cartes
+{
+    public enum Couleurs { Coeur, Pique, Carreau, Treffle}
+    public enum Valeurs { Deux = 2, Trois, Quatre, Cinq, Six, Sept, Huit, Neuf, Dix, Vallet, Reine, Roi, As}
+}
---
+++ Exo22 - Cartes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exo22___Cartes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carte[] deck = new Carte[52];
+
+            int i = 0;
+            foreach (string color_name in Enum.GetNames<Couleurs>())
+            {
+                foreach (string value_name in Enum.GetNames<Valeurs>())
+                {
+                    deck[i].couleur = Enum.Parse<Couleurs>(color_name);
+                    deck[i].valeur = Enum.Parse<Valeurs>(value_name);
+                    i++;
+                }
+            }
+
+            foreach (Carte carte in deck)
+            {
+                Console.WriteLine($"{carte.couleur} - {carte.valeur}");
+            }
+        }
+    }
+}
